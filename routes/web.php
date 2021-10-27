f129b670547ea70b1be7c32fa3a79a3d61cfbe49
Amit Gupta <40168699+amit728@users.noreply.github.com>
2021-10-27 23:20:37 +0530

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+ */
+
+Route::get('/', 'HomeController@index');
+
+Auth::routes(['register' => false, 'reset' => false]);
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/cashier', 'Cashier\CashierController@index');
+    Route::get('/cashier/getMenuByCategory/{category_id}',
+        'Cashier\CashierController@getMenuByCategory');
+    Route::get('/cashier/getTable', 'Cashier\CashierController@getTable');
+    Route::post('/cashier/orderFood', 'Cashier\CashierController@orderFood');
+    Route::get('/cashier/getSaleDetailByTable/{table_id}', 'Cashier\CashierController@getSaleDetailByTable');
+    Route::post('/cashier/confirmOrderStatus', 'Cashier\CashierController@confirmOrderStatus');
+    Route::post('/cashier/deleteSaleDetail', 'Cashier\CashierController@deleteSaleDetail');
+    Route::post('/cashier/savePayment', 'Cashier\CashierController@savePayment');
+    Route::get('/cashier/showReceipt/{saleID}', 'Cashier\CashierController@showReceipt');
+    Route::post('/cashier/increaseQuantity', 'Cashier\CashierController@increaseQuantity');
+    Route::post('/cashier/decreaseQuantity', 'Cashier\CashierController@decreaseQuantity');
+});
+
+Route::middleware(['auth', 'VerifyAdmin'])->group(function () {
+
+    Route::get('/management', function () {
+        return view('management.index');
+    });
+
+    // Routes for Management
+    Route::resource('management/category', 'Management\CategoryController');
+    Route::resource('management/menu', 'Management\MenuController');
+    Route::resource('management/table', 'Management\TableController');
+    Route::resource('management/user', 'Management\UserController');
+
+    // Routes for Report
+    Route::get('/report', 'Report\ReportController@index');
+    Route::get('/report/show', 'Report\ReportController@show');
+    Route::get('/report/show/export', 'Report\ReportController@export');
+});
